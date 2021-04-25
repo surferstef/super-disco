96406f50af13b120b15f9e5576b86a4d822c3d82
stefan hall <stefhall@comcast.net>
2021-04-25 15:18:42 -0700

    JS changes

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,7 +1,7 @@
 //Define DateTimestamp for today
 $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss a"));
 
-// For the button of each row -- each time clicked, need to loop back to the overarching parent id = 'hour1'
+// For the button of each row -- each time clicked, need to loop back to the overarching parent id = 'hour8' and on
     $("#saveBtn").on("click", function() {
         var currentHour = $(this)
             .parent()
@@ -15,36 +15,34 @@ $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss a"));
             localStorage.setItem(currentHour, descriptionText);
     })
 
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
-
+    
     function hours() {
         var currentMoment = moment().hour();
-
-        $(".hour-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-             
+         $(".hour-block").each(function () {
+            var blockTime = parseInt($(this)
+                .attr("id").split("hour")[1]);
+            //JSON.parse(localStorage.getItem($(this).attr("id").split("hour")[1]));  
             if (blockTime < currentMoment) {
                 $(this).addClass("past");
                    $(this).removeClass("future");
                    $(this).removeClass("present");
-            }
-            else if (blockTime === currentMoment) {
+            }   else if (blockTime === currentMoment) {
                 $(this).removeClass("past");
                     $(this).addClass("present");
                     $(this).removeClass("future");
-            }
-            else {
+            }    else {
                 $(this).removeClass("present");
                      $(this).removeClass("past");
                      $(this).addClass("future");
