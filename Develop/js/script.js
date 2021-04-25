44cbbf1a5001c59bf642c0e11ab4dd8928bad014
stefan hall <stefhall@comcast.net>
2021-04-25 10:53:27 -0700

    Initial Files

---
+++ Develop/js/script.js
@@ -0,0 +1,34 @@
+// $(".row").on("click", "p", function() {
+//     var text = $(this)
+//     .text()
+//     .trim();
+//     console.log(text);
+//     var textInput = $("<textarea>")
+//       .addClass("form-control")
+//       .val(text);
+//     $(this).replaceWith(textInput).textInput.trigger("focus");
+
+
+// $(".row").on("blur", "textarea", function() {
+//     //get the text area's current value/text
+//     var text = $(this)
+//       .val()
+//       .trim();
+//     // get the parent ul's id attribute
+//     var status = $(this)
+//       .closest(".description")
+//       .attr("id")
+//       .replace("list-", "");
+//   });
+//   // get the task's position in the list of other li elements
+//   var index = $(this)
+//   .closest(".description-item")
+//   .index();
+//   //recreate p element
+//   var taskP = $("<p>")
+//   .addClass("m-1")
+//   .text(text);
+
+//   //replace textarea with p element
+//   $(this).replaceWith(taskP);
+// });
